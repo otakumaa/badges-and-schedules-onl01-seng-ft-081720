c8181483ff2b8bbe7d40eeceafca52ffa1facc0b
Bryce Ancheta <bryceancheta@gmail.com>
2020-08-19 02:32:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,4 +16,8 @@ def assign_rooms(array)
   new_array = []
   counter = 1
   array.each do |name|
-    new_array.push("hello, #{name}! ")
+    new_array.push("hello, #{name}! You'll be assigned to room #{counter}!")
+    counter += 1
+  end
+  new_array
+end
